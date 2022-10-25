092b16e8735271be446fbe583d76cd1500693561
Shaikh Vakas Ahmed <97548328+Vakasahmed@users.noreply.github.com>
2022-10-25 13:40:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 import Nav from './Components/Nav';
 import Home from './Pages/Home';
-import Footer from './Components/Footer
 
 function App() {
   return (
     <main>
       <Nav />
        <Home />
-    <Footer/>
     </main>
 
   );
