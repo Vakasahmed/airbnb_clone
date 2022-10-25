9763324a9974fa302b8d10a8e1053f862e248964
Shaikh Vakas Ahmed <97548328+Vakasahmed@users.noreply.github.com>
2022-10-25 10:05:06 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Nav from './Components/Nav';
+import Home from './Pages/Home';
+
+function App() {
+  return (
+    <main>
+      <Nav />
+       <Home />
+    </main>
+
+  );
+}
+
+export default App;
